7ddd7edb1368e2121d2cc222660bba5a647db3eb
codie0226 <legoholic226@naver.com>
2026-01-21 00:00:52 +0900

    feat(test): 테스트 api에서 인증 토큰 발급

--- src/controller/test_controller.ts
+++ src/controller/test_controller.ts
@@ -1,10 +1,29 @@
-import {Get, Tags, Route} from 'tsoa';
+import { Get, Tags, Route, SuccessResponse } from 'tsoa';
+import prisma from '../config/prisma';
+import { TsoaSuccessResponse } from '../config/response_interface';
+import { generateTokens } from '../config/jwt';
 
 @Route('api/test')
 @Tags('Test')
 export class TestController {
+  /**
+   *
+   * @returns 테스트 유저의 액세스 토큰
+   */
   @Get('/')
-  public async getTest(): Promise<string> {
-    return 'Hello from Test Controller!';
+  @SuccessResponse('200', 'Success')
+  public async getTest(): Promise<TsoaSuccessResponse<string>> {
+    const testUser = await prisma.user.findFirst({
+      where: {
+        user_name: 'test_user',
+      },
+    });
+
+    const { accessToken } = await generateTokens({
+      id: testUser.user_id,
+      email: testUser.email,
+    });
+
+    return new TsoaSuccessResponse(accessToken);
   }
 }
