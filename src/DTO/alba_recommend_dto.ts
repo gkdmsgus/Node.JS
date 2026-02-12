d68d016b9704f1a3cbb4bddfdd17765053007625
Jinacker <rlawls1448@naver.com>
2026-02-12 20:47:43 +0900

    feat(alba_recommend): 알바 추천 기능 구현

---
+++ src/DTO/alba_recommend_dto.ts
@@ -0,0 +1,10 @@
+export interface RecommendedAlbaDto {
+  albaId: string;
+  storeName: string | null;
+  storeAddress: string | null;
+  hourlyRate: number | null;
+  totalWage: number | null;
+  mainTask: string | null;
+  regionCity: string | null;
+  regionDistrict: string | null;
+}
